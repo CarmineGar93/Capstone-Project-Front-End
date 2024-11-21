ab429b37e6cc52ee54d31ae44e793d1829abb6ef
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-21 18:42:13 +0100

    offer title inserted

--- capstone/src/components/home/Home.jsx
+++ capstone/src/components/home/Home.jsx
@@ -3,9 +3,13 @@ import HomeHero from "./HomeHero"
 
 function Home() {
     return (
-        <Container fluid className="px-lg-3 px-xl-6 px-xxl-10 custom-background">
-            <HomeHero />
-        </Container>
+        <>
+            <Container fluid className="px-lg-3 px-xl-6 px-xxl-10 custom-background">
+                <HomeHero />
+            </Container>
+            <h2 className="my-5 text-center">What's offering (siteName)</h2>
+        </>
+
     )
 }
 
