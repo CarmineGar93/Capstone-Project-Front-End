0c5f3c8677540e0d3317e2e0042f688b032940d3
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-21 15:51:54 +0100

    struttura navbar completata

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './customized.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MyNavbar from './components/MyNavbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <MyNavbar></MyNavbar>
       <Routes>
         <Route>
 
---
+++ capstone/src/components/MyNavbar.jsx
@@ -0,0 +1,46 @@
+import { Navbar, Container, Offcanvas, Nav, Button } from 'react-bootstrap'
+function MyNavbar() {
+    return (
+        <>
+            <Navbar key='lg' expand='lg' className="bg-body-tertiary mb-3 py-2">
+                <Container fluid className='mx-lg-2 mx-xl-8 mx-xxl-9'>
+                    <Navbar.Brand href="#home" className='py-2 fs-4 me-0'>
+                        <img
+                            alt=""
+                            src="http://placedog.net/50/50"
+                            width="40"
+                            height="40"
+                            className="d-inline-block align-top rounded-circle"
+                        />{' '}
+                        Webname
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+                    <Navbar.Offcanvas
+                        id={`offcanvasNavbar-expand-lg`}
+                        aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+                        placement="start"
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
+                                Menu
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-center flex-grow-1 pe-3">
+                                <Nav.Link href="#action1" className='pe-lg-3 pe-xl-4 pe-xxl-5 fs-5'>Home</Nav.Link>
+                                <Nav.Link href="#action2" className='pe-lg-3 pe-xl-4 pe-xxl-5 fs-5'>Your Products</Nav.Link>
+                                <Nav.Link href="#action3" className='pe-lg-3 pe-xl-4 pe-xxl-5 fs-5'>Recepies</Nav.Link>
+                                <Nav.Link href='#action4' className='pe-lg-3 pe-xl-4 pe-xxl-5 fs-5'>Weekly Plans</Nav.Link>
+                                <Nav.Link href='#action5' className='pe-lg-3 pe-xl-4 pe-xxl-5 fs-5'>Your grocery lists</Nav.Link>
+                            </Nav>
+                            <Button variant='dark' className='mt-3 mt-lg-0'>Login</Button>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container >
+            </Navbar >
+
+        </>
+    )
+}
+
+export default MyNavbar
