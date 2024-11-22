a765a68039c1847313bcbb7f8c148684fc1ee84a
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-22 12:52:40 +0100

    footer template

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -3,14 +3,16 @@ import './customstyle.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MyNavbar from './components/MyNavbar';
 import Home from './components/home/Home';
+import MyFooter from './components/MyFooter';
 
 function App() {
   return (
     <BrowserRouter>
-      <MyNavbar></MyNavbar>
+      <MyNavbar />
       <Routes>
         <Route path='/*' element={<Home />} />
       </Routes>
+      <MyFooter />
     </BrowserRouter>
   );
 }
---
+++ capstone/src/components/MyFooter.jsx
@@ -0,0 +1,41 @@
+import { Container, Row, Col } from "react-bootstrap"
+import { HouseDoorFill, EnvelopeAtFill, TelephoneFill, Facebook, TwitterX, Google, Instagram } from 'react-bootstrap-icons'
+
+function MyFooter() {
+    return (
+        <footer className="bg-body-tertiary">
+            <Container className="p-4 pb-0">
+                <Row>
+                    <Col md={6} className=" mx-auto mt-3 text-center text-lg-start ">
+                        <img src="http://placedog.net/100/100" alt="" className="img-fluid mb-4" />
+                        <h6>siteName</h6>
+                    </Col>
+                    <hr className="w-100 clearfix d-md-none" />
+                    <Col md={6} className=" mx-auto mt-3 text-center text-lg-end">
+                        <h6 className="mb-4">CONTACT</h6>
+                        <p><HouseDoorFill /> Via IP 23, Sorrento 80067, NA</p>
+                        <p><EnvelopeAtFill /> info@siteName.com</p>
+                        <p><TelephoneFill /> + 39 081 8043457</p>
+                    </Col>
+                </Row>
+                <hr className="my-3" />
+                <Row className="py-3 pt-0 d-flex align-items-center">
+                    <Col md={7} lg={8} className="text-center text-md-start">
+                        <div>
+                            © 2020 Copyright:
+                            <a href="#u">Carmine.com</a>
+                        </div>
+                    </Col>
+                    <Col md={5} lg={4} className="text-center text-md-end">
+                        <a href="#u" className="btn btn-floating m-1" role="button" ><Facebook /></a>
+                        <a href="#u" className="btn btn-floating m-1" role="button"><TwitterX /></a>
+                        <a href="#u" className="btn btn-floating m-1" role="button"><Google /></a>
+                        <a href="#u" className="btn btn-floating m-1" role="button"><Instagram /></a>
+                    </Col>
+                </Row>
+            </Container>
+        </footer >
+    )
+}
+
+export default MyFooter
