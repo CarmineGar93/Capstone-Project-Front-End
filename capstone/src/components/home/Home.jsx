ae0baf1fb12c8201ade509293445f94f96262040
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-26 18:37:56 +0100

    products check component

--- capstone/src/components/home/Home.jsx
+++ capstone/src/components/home/Home.jsx
@@ -3,13 +3,14 @@ import HomeSidebar from "./HomeSidebar";
 import ProfileCard from "./left/ProfileCard";
 import GroceryPanel from "./left/GroceryPanel";
 import CalendarPlan from "./right/CalendarPlan";
+import ProductsCheck from "./right/ProductsCheck";
 function Home() {
     return (
         <Container className="my-5">
             <Row className="gx-3">
                 <Col xs={3}><HomeSidebar child1={<ProfileCard />} child2={<GroceryPanel />} /></Col>
                 <Col xs={6}></Col>
-                <Col xs={3}><HomeSidebar child1={<CalendarPlan />} child2={<GroceryPanel />} /></Col>
+                <Col xs={3}><HomeSidebar child1={<CalendarPlan />} child2={<ProductsCheck />} /></Col>
             </Row>
         </Container>
     )
---
+++ capstone/src/components/home/right/ProductsCheck.jsx
@@ -0,0 +1,29 @@
+import { Alert, Card, Button } from "react-bootstrap"
+
+function ProductsCheck() {
+    const check = false;
+
+    return (
+        <Card className="border-0">
+            <h5 className="mb-3 bg-white shadow-sm p-3 text-center">Your products</h5>
+            <div className="p-3">
+                {
+                    check ? (
+                        <Alert variant="danger" className="mb-0">
+                            Some products are missing to perform all your weekly recepies
+                        </Alert>
+                    ) : (
+                        <Alert variant="success" className="mb-0">
+                            Your products are aligned with your weekley plan
+                        </Alert>
+                    )
+                }
+            </div>
+            <div className="text-center p-3">
+                <Button variant="danger">Products management</Button>
+            </div>
+        </Card>
+    )
+}
+
+export default ProductsCheck
