0947a9af1fb41a5ac53db78b3909fbe500cc72d1
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-28 18:40:56 +0100

    weekly plans central part completed

--- capstone/src/components/plans/WeeklyPlans.jsx
+++ capstone/src/components/plans/WeeklyPlans.jsx
@@ -1,12 +1,36 @@
 import { Col, Container, Row } from "react-bootstrap"
 import WeeklyPlansLeft from "./WeeklyPlansLeft"
+import WeeklyPlanChoosen from "./WeeklyPlanChoosen"
+import { useState } from "react"
 
 function WeeklyPlans() {
+    const plans = [{
+        id: 1,
+        start: "25 Nov",
+        end: "1 Dec",
+        state: "ACTIVE"
+    }, {
+        id: 2,
+        start: "2 Dec",
+        end: "8 Dec",
+        state: "IN_PROGRAM"
+    }, {
+        id: 3,
+        start: "18 Nov",
+        end: "24 Nov",
+        state: "EXPIRED"
+    }]
+    const stateColors = {
+        ACTIVE: "success",
+        IN_PROGRAM: "warning",
+        EXPIRED: "secondary"
+    }
+    const [selected, setSelected] = useState(plans.filter(plan => plan.state === "ACTIVE")[0])
     return (
         <Container className="my-5">
             <Row className="g-3">
-                <Col xs={3}><WeeklyPlansLeft /></Col>
-                <Col xs={9}></Col>
+                <Col xs={3}><WeeklyPlansLeft selected={selected} changeSelected={setSelected} plans={plans} badgeColors={stateColors} /></Col>
+                <Col xs={9}><WeeklyPlanChoosen selected={selected} badgeColors={stateColors} /></Col>
             </Row>
         </Container>
     )
--- capstone/src/components/plans/WeeklyPlansLeft.jsx
+++ capstone/src/components/plans/WeeklyPlansLeft.jsx
@@ -1,27 +1,34 @@
 import { Badge, Button, Card } from "react-bootstrap"
 import { Calendar } from "react-bootstrap-icons"
 
-function WeeklyPlansLeft() {
+function WeeklyPlansLeft({ selected, changeSelected, plans, badgeColors }) {
     return (
-        <Card className="border-0 p-4">
-            <div className="d-flex align-items-center mb-4">
-                <Calendar size={20} className="me-2" />
-                <h4 className="mb-0">Weekly Plans List</h4>
-            </div>
-            <div className="mb-5">
-                <div className=" border-start border-3 px-2 mb-3">
-                    <h6 className="text-secondary">List 1: 25 Nov - 1 Dec <Badge bg={"success"}>Active</Badge></h6>
-                </div>
-                <div className=" border-start border-3 px-2 mb-3">
-                    <h6 className="text-secondary">List 2: 2 Dec - 8 Dec <Badge bg={"warning"}>In program</Badge></h6>
-                </div>
-                <div className=" border-start border-3 px-2">
-                    <h6 className="text-secondary">List 3: 18 Nov - 24 Nov <Badge bg={"secondary"}>Expired</Badge></h6>
-                </div>
-            </div>
-            <div className="mb-">
-                <Button variant="danger">Create new plan</Button>
-            </div>
+        <Card className="border-0 p-4 h-100">
+            {
+                selected && (
+                    <>
+                        <div className="d-flex align-items-center mb-4">
+                            <Calendar size={20} className="me-2" />
+                            <h4 className="mb-0">Weekly Plans</h4>
+                        </div>
+                        <div className="mb-5">
+                            {
+                                plans.map(plan => {
+                                    return (
+                                        <div className={`border-start border-3 p-2 mb-3 align-content-center ${selected.id === plan.id ? "border text-info rounded-2" : ""}`} onClick={() => changeSelected(plan)}>
+                                            <h6 className="text-secondary mb-0">{plan.start} - {plan.end} <Badge bg={badgeColors[plan.state]}>{plan.state}</Badge></h6>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div className="mt-auto">
+                            <Button variant="danger">Create new plan</Button>
+                        </div>
+                    </>
+                )
+            }
+
 
 
 
