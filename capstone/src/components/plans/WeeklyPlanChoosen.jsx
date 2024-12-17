a1f1610586c914c521687a6a7e3ca1c33bc0c77e
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-12-17 19:04:26 +0100

    removed comment

--- capstone/src/components/plans/WeeklyPlanChoosen.jsx
+++ capstone/src/components/plans/WeeklyPlanChoosen.jsx
@@ -151,63 +151,6 @@ function WeeklyPlanChoosen({ selected, badgeColors }) {
                                 })
                             }
                         </div>
-                        {/*   <Accordion flush className="mb-3">
-                            {
-                                selected.dailyPlans.map(daily => {
-                                    return (
-
-                                        <Accordion.Item eventKey={daily.dailyPlanId} className="border-0 border-bottom" key={daily.dailyPlanId}>
-                                            <OverlayTrigger placement="right" delay={{ show: 200 }} overlay={<Tooltip >Tot calories: {daily.totCalories.toFixed(2)} kcal</Tooltip>}>
-                                                <Accordion.Header><h5 className="mb-0">{days[daily.day]} {valuateToday(selected.startDate, daily.day) && <Badge bg="danger" pill className="px-2 py-0 rounded-circle ms-1">Today</Badge>}</h5></Accordion.Header></OverlayTrigger>
-                                            <Accordion.Body className="px-2 py-4">
-                                                <Accordion>
-                                                    {
-                                                        daily.meals.map(meal => {
-                                                            return (
-                                                                <>
-                                                                    {
-                                                                        meal.recipe ? (
-                                                                            <Accordion.Item className="border-0 border-bottom mb-2" key={meal.mealId} eventKey={meal.mealId}>
-                                                                                <Accordion.Header><p className="mb-0 fs-5 px-0">{meal.type.toLowerCase().replace(/^./, char => char.toUpperCase())}</p></Accordion.Header>
-                                                                                <Accordion.Body>
-                                                                                    <div className="d-flex justify-content-between align-items-center">
-                                                                                        <div className="d-flex align-items-center">
-                                                                                            <img src={meal.recipe.imageUrl} alt="dish" width={60} height={40} className="me-2" />
-                                                                                            <p className="mb-0 recipe-link" onClick={() => navigate(`/recipe/${meal.recipe.reference}`)}>{meal.recipe.name} <BoxArrowInUpRight className="mb-1"></BoxArrowInUpRight></p>
-                                                                                        </div>
-                                                                                        {
-                                                                                            !valuateDate(selected.startDate, daily.day) && <Button variant="danger" onClick={() => removeReceipt(meal.mealId)}><Trash className="py-0"></Trash></Button>
-                                                                                        }
-
-
-                                                                                    </div>
-
-                                                                                </Accordion.Body>
-                                                                            </Accordion.Item >
-                                                                        ) : (
-                                                                            <div className="border-bottom border-1 d-flex justify-content-between align-items-center mb-2 px-3" key={meal.mealId}>
-                                                                                <p className="mb-0 fs-5">{meal.type.toLowerCase().replace(/^./, char => char.toUpperCase())}</p>
-                                                                                <button className={`${!valuateDate(selected.startDate, daily.day) && "btn-experience"} bg-white border-0 rounded-circle p-2`} onClick={() => {
-                                                                                    setMealSelected(meal.mealId)
-                                                                                    setShow(true)
-                                                                                }} disabled={valuateDate(selected.startDate, daily.day)}><Plus size={25} /></button>
-                                                                            </div>
-                                                                        )
-                                                                    }
-                                                                </>
-
-                                                            )
-                                                        })
-                                                    }
-                                                </Accordion>
-                                            </Accordion.Body>
-                                        </Accordion.Item>
-
-
-                                    )
-                                })
-                            }
-                        </Accordion> */}
                     </Card >
                 )
             }
